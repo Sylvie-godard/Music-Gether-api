4c61c03055beb3867326e1d28ace6ba8e6541419
Sylvie Godard <sylvie.godard@coding-academy.fr>
2020-03-01 21:48:02 +0100

    [ADD] user entity and repository

--- src/Controller/ConcertController.php
+++ src/Controller/ConcertController.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ConcertController
 {
-
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): Response
     {
-        var_dump($request->request->all);
+        $response = $request->request->all();
+
+        var_dump($response);
+        return new Response();
     }
 }
--- src/Entity/Concert.php
+++ src/Entity/Concert.php
@@ -8,7 +8,10 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Cake\Chronos\ChronosInterface; 
 
-
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ConcertRepository")
+ */
 class Concert
 {
     /**
